963d076220ab55e3a2e792dd1f2f4442f6e098dd
E-963 <enanysama10@gmail.com>
2024-12-06 23:24:27 +0200

    Configure Provider

--- configureProvider.tf
+++ configureProvider.tf
@@ -1,10 +1,17 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.58.0"
-    }    
+      source = "hashicorp/aws"
+      version = "5.80.0"
+    }
   }
+
+    backend "s3" {
+    bucket = "sky-s3-main10"
+    key    = "terraform/states/frogtech-TG/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+    }
 
   # cloud {
   #   organization = "DevopsKitchenWorkshop"
@@ -18,4 +25,5 @@ terraform {
 # Configure aws provider
 provider "aws" {
   region = "us-east-1"
+  profile = "default"
 }
