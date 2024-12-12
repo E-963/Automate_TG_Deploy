97590426f4451667669177218233a6b2773434f1
E-963 <enanysama10@gmail.com>
2024-12-12 11:58:37 +0200

    Update CircleCi Auto TransitGateway

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "5.60"
+      source  = "hashicorp/aws"
+      version = "5.60.0" // Make sure this version exists
     }
   }
 
