e10d8eb4740925b4b76ff2b1d8b8a9092033c9de
E-963 <enanysama10@gmail.com>
2024-12-14 13:02:24 +0200

    Updating EC2 File

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 # Create key pair for SSH access into EC2 instances
-resource "aws_key_pair" "ssh_key_1"{
-  key_name  = "ssh_key_1"
-  public_key = file("/home/username/.ssh/ssh_key_1.pub")
+resource "aws_key_pair" "key_1"{
+  key_name  = "key_1"
+  public_key = file("~/.ssh/key_1.pub")
 }
 
 # EC2-A Instance in VPC_A for Testing
@@ -12,7 +12,7 @@ resource "aws_instance" "EC2_A_testing"{
     associate_public_ip_address   = true
     subnet_id              = aws_subnet.vpc_A_private_subnet.id
     vpc_security_group_ids = [aws_security_group.vpc_A_secgrp.id]
-    key_name               = aws_key_pair.ssh_key_1.key_name
+    key_name               = aws_key_pair.key_1.key_name
 
     tags = {
     Name        = "EC2_A_testing"
@@ -29,7 +29,7 @@ resource "aws_instance" "EC2_B_testing"{
     instance_type          = "t2.micro"
     subnet_id              = aws_subnet.vpc_B_private_subnet.id
     vpc_security_group_ids = [aws_security_group.vpc_B_secgrp.id]
-    key_name               = aws_key_pair.ssh_key_1.key_name    # use the same key
+    key_name               = aws_key_pair.key_1.key_name    # use the same key
 
     tags = {
     Name        = "EC2_B_testing"
@@ -46,7 +46,7 @@ resource "aws_instance" "bastion-host" {
   ami                    = "ami-0a0e5d9c7acc336f1" # Amazon Ubuntu 22.04 AMI in us-east-1
   instance_type          = "t3.micro"
   subnet_id              = aws_subnet.vpc_c_public_subnet.id
-  key_name                = aws_key_pair.ssh_key_1.key_name
+  key_name                = aws_key_pair.key_1.key_name
   vpc_security_group_ids = [aws_security_group.bastion-host-sg.id]
 
   tags = {
